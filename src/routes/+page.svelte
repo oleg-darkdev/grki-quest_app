1b674b70fcdb5efdbc6356c9fd57de790b985149
DarkDev <darkdev_brand@riseup.net>
2024-07-19 10:05:15 +0300

    intial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	// import {  } from '$lib/shared';
-	// import {  } from '$lib/widgets';
+	import { Footer, Nav, HeroType1, FullWidthImg, Features, Locations, Stats, DividerApp, Faq, RoutePoints, DividerType2 } from '$lib/widgets';
   // import {  } from '$lib/entities'
 </script>
 
@@ -9,6 +9,81 @@
   <title>Title</title>
 </svelte:head>
 
+
+<Nav />
+
+<HeroType1 />
+
+<FullWidthImg />
+
+
+
+<Locations />
+
+
+<!-- <DividerType2 /> -->
+
+
+<!-- <div class="section blue main">
+  <div class="container">
+    <div class="wrapper main_cutout">
+      <div class="row fill">
+        <div class="col_50">
+          <div class="img_container">
+            <img src="images/63a022bf88734f151f436d97_Gothenburg%20Central%20Station.jpg" loading="lazy" width="465" sizes="(max-width: 767px) 90vw, 44vw" alt="Vision of Gothenburg Central Station with Grass" srcset="images/63a022bf88734f151f436d97_Gothenburg%20Central%20Station-p-500.jpg 500w, images/63a022bf88734f151f436d97_Gothenburg%20Central%20Station-p-800.jpg 800w, images/63a022bf88734f151f436d97_Gothenburg%20Central%20Station-p-1080.jpg 1080w, images/63a022bf88734f151f436d97_Gothenburg%20Central%20Station-p-1600.jpg 1600w, images/63a022bf88734f151f436d97_Gothenburg%20Central%20Station.jpg 2204w" class="img_cover">
+            <div class="location_tag_curve">
+              <img src="images/63d50347d8cef93927c52327_end_curve.svg" loading="lazy" alt="" class="img_curve_end">
+              <p class="location_tag">Vision: Gothenburg, Central Station, Sweden</p>
+              <img src="images/63d50347f7e09bde3c233af0_start_curve.svg" loading="lazy" alt="" class="img_curve_start">
+            </div>
+          </div>
+        </div>
+        <div class="col_50">
+          <div class="col_wrap middle">
+            <div class="tag_white">GREENER CITIES</div>
+            <h3>We make green cities possible</h3>
+            <p>Our Capillary Hydroponics solution turns hardscaped surfaces into breathable, living spaces that augment the positive effects of greenery in urban environments.&nbsp;</p>
+            <div class="row gap__4 align__left">
+              <div class="tag _4px">Binds air pollution</div>
+              <div class="tag _4px">Lowers noise</div>
+              <div class="tag _4px">Lowers temperature</div>
+              <div class="tag _4px">Decreases flooding</div>
+            </div>
+            <div class="wrap_buttons">
+              <a data-w-id="d1f65f60-9d67-cbb2-ee70-57a00c2bf799" href="/about" class="cta-container w-inline-block">
+                <div class="cta__text">Our vision</div>
+                <img src="images/63a183bd0c5c8e5ee8773811_arrow-right-white.svg" loading="lazy" alt="" class="cta__arrow">
+              </a>
+              <a data-w-id="95e2f79a-1194-5cbc-4d35-6361162e02e5" href="/hydroponics" class="cta-container secondary white-hover w-inline-block">
+                <div class="cta__text">Capillary Hydroponics</div>
+                <img src="images/63a183bd18ceba00f0e0f78a_arrow-right-black.svg" loading="lazy" alt="" class="cta__arrow">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> -->
+
+
+
+<DividerApp />
+
+<a name="track"></a>
+
+<Stats />
+
+<RoutePoints />
+
+<!-- <Features /> -->
+
+
+
+<Faq />
+
+
+<Footer />
 
 <style>
 
