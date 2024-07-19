52fcd01bd19be8b6838dd712d5dd2a652208fe57
DarkDev <darkdev_brand@riseup.net>
2024-07-19 10:05:04 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Stats.svelte
@@ -0,0 +1,46 @@
+<!-- <script>
+	import { stats } from '$lib/shared';
+</script> -->
+
+ <section class="section">
+  <div class="container outline">
+    <div class="column gap__16">
+      <div class="row fill">
+        <div>
+          <div class="col_wrap">
+            <h3>Lorem Ipsum is simply dummy.</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
+        </div>
+      </div>
+      <div class="divider"></div>
+      <div class="row">
+        <div  class="col_wrap">
+          <div class="grid_why_cf">
+            <div  class="card_wrap">
+              <h3>7.4KM</h3>
+              <p class="subhead">Trasa przez las</p>
+            </div>
+            <div class="card_wrap">
+              <h3>4.2KM</h3>
+              <p class="subhead">Trasa wzdłuż morza</p>
+            </div>
+            <div class="card_wrap green">
+              <h3>
+                <span class="highlight_text">17</span>
+              </h3>
+              <p class="subhead">punktów na mapie</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p class="paragraph">*Lorem Ipsum<sup>2</sup>
+        <br>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </p>
+      <a  href="/app" class="cta-container w-inline-block">
+        <div class="cta__text">Przejdź do trasy</div>
+        <img src="images/63a183bd0c5c8e5ee8773811_arrow-right-white.svg" loading="lazy" alt="" class="cta__arrow">
+      </a>
+    </div>
+  </div>
+</section>
