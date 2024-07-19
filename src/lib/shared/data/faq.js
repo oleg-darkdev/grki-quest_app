2302cb0f4256e800f219ba7a1ae1ba1f35174e2c
DarkDev <darkdev_brand@riseup.net>
2024-07-19 10:08:07 +0300

    intial

---
+++ src/lib/shared/data/faq.js
@@ -0,0 +1,29 @@
+  const faqData = [
+    {
+      q: 'Lorem Ipsum is simply dummy text of the printing',
+      a: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`, `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`]
+    },
+    {
+      q: 'Lorem Ipsum is simply dummy text of the printing',
+      a: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`, `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`]
+    },
+    {
+      q: 'Lorem Ipsum is simply dummy text of the printing',
+      a: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`, `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`]
+    },
+    {
+      q: 'Lorem Ipsum is simply dummy text of the printing',
+      a: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`, `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`]
+    },
+    {
+      q: 'Lorem Ipsum is simply dummy text of the printing',
+      a: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`, `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`]
+    },
+    {
+      q: 'Lorem Ipsum is simply dummy text of the printing',
+      a: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`, `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`]
+    },
+]
+
+
+export default faqData;
