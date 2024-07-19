9565a140bc9b87b1de41a046e186ee2b29008430
DarkDev <darkdev_brand@riseup.net>
2024-07-19 10:05:31 +0300

    refactoring structure data

---
+++ src/lib/shared/data/route.js
@@ -0,0 +1,76 @@
+// nature
+import amberlake from './locations/amberlake';
+import nudeBeach from './locations/nudeBeach';
+import firstHill from './locations/firstHill';
+import forest from './locations/forest';
+import greenIslands from './locations/greenIslands';
+import birdsParadise from './locations/birdsParadise';
+import birdsBeach from './locations/birdsBeach';
+import marineIsland from './locations/marineIsland';
+
+// historical
+import przełomStreet from './locations/przełomStreet';
+import shipyard from './locations/shipyard';
+import submarine from './locations/submarine';
+import bunkier_1 from './locations/bunkier_1';
+import hedgehog from './locations/hedgehog';
+import anchor from './locations/anchor';
+import lighthouse from './locations/lighthouse';
+import pirs from './locations/pirs';
+import bunkier_3 from './locations/bunkier_3';
+import bunkier_4 from './locations/bunkier_4';
+import bunkier_5 from './locations/bunkier_5';
+
+// other
+import fisherman from './locations/fisherman';
+
+const allArr = [
+  firstHill,
+  shipyard,
+  przełomStreet,
+  submarine,
+  bunkier_1,
+  fisherman,
+  forest,
+  hedgehog,
+  greenIslands,
+  anchor,
+  lighthouse,
+  birdsParadise,
+  pirs,
+  nudeBeach,
+  bunkier_3,
+  bunkier_4,
+  birdsBeach,
+  marineIsland,
+  bunkier_5,
+  amberlake,
+]
+
+const historicalGroup = [
+  przełomStreet,
+  shipyard,
+  submarine,
+  bunkier_1,
+  hedgehog,
+  anchor,
+  lighthouse,
+  pirs,
+  bunkier_3,
+  bunkier_4,
+  bunkier_5,
+]
+
+const natureGroup = [
+  amberlake,
+  nudeBeach,
+  firstHill,
+  forest,
+  greenIslands,
+  birdsParadise,
+  birdsBeach,
+  marineIsland,
+]
+
+
+export {allArr, natureGroup, historicalGroup,};
