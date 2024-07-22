79ad8e10fc45841f63111f74b3be4143861a4877
DarkDev <darkdev_brand@riseup.net>
2024-07-22 11:03:25 +0300

    add route for tests

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,1 +1,15 @@
+<script>
+	// import {  } from '$lib/shared';
+	import { Free1, Free2, Free3, Free4, Free5, Free6, Free7 } from '$lib/widgets';
+  // import {  } from '$lib/entities'
+</script>
+
+
+<Free1/>
+<Free2 />
+<Free3/>
+<Free4/>
+<Free5 />
+<Free6 />
+<Free7 />
 
